a6eb678cde7deca72a5dc1d41993e3a91a314bd4
stx <xiongwenyan@gmail.com>
2026-05-15 18:25:37 +0800

    use tertiary operator instead of if

--- 18-React/02-React进阶/02-github搜索/components/List/index.jsx
+++ 18-React/02-React进阶/02-github搜索/components/List/index.jsx
@@ -4,22 +4,15 @@ import Item from '../Item'
 export default class List extends Component {
   render() {
     let { user, isFirst, isLoading, error } = this.props
-    if (isFirst) {
-      return <h2>请输入关键词以搜索用户</h2>
-    } else if (isLoading) {
-      return <h2>Loading...</h2>
-    } else if (error) {
-      return <h2>{error}</h2>
-    } else {
-      return (
-        <div className="row">
-          {
-            user.map((item) => {
-              return <Item key={item.login} {...item} />
-            })
-          }
-        </div>
-      )
-    }
+    return <div className="row">
+      {
+        isFirst ? <h2>请输入关键词以搜索用户</h2> :
+          isLoading ? <h2>Loading...</h2> :
+            error ? <h2>{error}</h2> :
+              user.map((item) => {
+                return <Item key={item.login} {...item} />
+              })
+      }
+    </div>
   }
 }
