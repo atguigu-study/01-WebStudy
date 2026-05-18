fbe0b112ec750cf585de0fdc809da601f294c14c
stx <xiongwenyan@gmail.com>
2026-05-18 21:54:25 +0800

    redux异步action

--- 18-React/02-React进阶/12-Redux/03-异步action/redux/actions.js
+++ 18-React/02-React进阶/12-Redux/03-异步action/redux/actions.js
@@ -1,13 +1,9 @@
-/**
- * 该文件用于创建action对象
- */
-
 import { INCREMENT, DECREMENT } from './constant'
 
-// 加
-export const incrementAction = (data) => {
-  return { type: INCREMENT, data }
-}
+export const incrementAction = data => ({ type: INCREMENT, data })
+
+export const decrementAction = data => ({ type: DECREMENT, data })
+
 // 异步加
 /**
  * 异步action中，返回值为函数，其中一般会调用同步action
@@ -18,8 +14,4 @@ export const incrementAsyncAction = (data, delay) => {
       dispatch(incrementAction(data))
     }, delay)
   }
-}
-// 减
-export const decrementAction = (data) => {
-  return { type: DECREMENT, data }
 }
